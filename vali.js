251649be4d094ac93d83677bddf8a9af2be1cae6
Hirthik Pamnani <88090276+hirthikpamnani@users.noreply.github.com>
2021-10-01 14:30:09 +0530

    Update vali.js

--- vali.js
+++ vali.js
@@ -1,25 +1,39 @@
-function validate(number,address)
-{
-   var regEx = /^\d{10}$/;
-   var  nullvalue =/^$|\s+/;
-   if(!number.value.match(regEx))
-     {
-      alert("Please enter valid phone number!");
-     return false;
-     }
-    else if(address.value.match(nullvalue)) {
-       alert("Please enter address! ");
-       return false;
-       
+function validateForm() {
+
+       var a = /^[A-Za-z ]{3,30}$/
+       var b = /^[0-99]{2}$/
+       var c = /^[789]\d{9}$/
+       if(a.test(document.myForm.tagline.value)==false)
+       {
+        alert("Enter Valid Tagline!");
+        }
+       else if(b.test(document.myForm.quantity.value)==false)
+       {
+	alert("Enter Quantity between 1 to 99");
+	}
+       else if(size =="select a size")
+       {
+	alert("Enter a size");
+	}
+       else if(address== "")
+       {
+	alert("Enter valid address");
+	}
+       else if(c.test(document.myForm.number.value)==false)
+       {
+	alert("Enter valid Phone number");
+	}
+       else
+       {
+        var tagline= document.getElementById("tagline").value;
+        var name= document.getElementById("name").value;
+        var number = document.getElementById("number").value; 
+        var address = document.getElementById("address").value; 
+        var size = document.getElementById("size").value;
+	
+	       var quantity =document.getElementById("quantity").value;
+	       var total=quantity*1500;
+        alert("Order Recepit \nName: "+name+ "\nPhone Number: "+number+ "\nTagline: "+tagline+ "\nAddress: "+address+ "\nSize: "+size+ "\nTotal Amount:"+total+"."
+	     );
        }
-   else {
-      alert("Form submitted"+address.value);
-      }
-
-    if(address===null || address==" "){
-
-     alert("Please enter address before placing order!");
-      return false;
-     }
-    
-}  
+}
